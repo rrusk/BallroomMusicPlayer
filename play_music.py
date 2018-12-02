3316751efdae7b752bf747ebf04c7dfcaa23d17b
Raymond Rusk <Raymond.Rusk@shaw.ca>
2018-12-02 13:45:48 -0800

    Making more modular

--- play_music.py
+++ play_music.py
@@ -34,20 +34,6 @@ def is_intString(s):
         return True
     except ValueError:
         return False
-    
-if len(sys.argv) > 2:
-    print "Script to play dance music for practice"
-    print "Usage: "+sys.argv[0]+" [number]"
-    print "  [number] is musical selections per dance."
-    print "  Default is 2 if no argument provided."
-    exit(1)
-elif len(sys.argv) == 2:
-    if not is_intString(sys.argv[1]):
-        print "The number of musical selection per dance must be an integer."
-        exit(1)
-    numSel = int(sys.argv[1])
-else:
-    numSel = 2
 
 def randomList(a):
     b=[]
@@ -65,64 +51,83 @@ def displayInfo(player):
     album=media.get_meta(vlc.Meta.Album) or "Unknown album"
     return title+"-"+artist+"-"+album
 
-home = expanduser("~")
-musicDir=home+"/Music/"
+def play_music(numSel):
+    numSel = numSel
+    home = expanduser("~")
+    musicDir=home+"/Music/"
 
-#dances = ["QuickStep","PasoDoble","Jive"]
-dances = ["Waltz","Tango","VienneseWaltz","QuickStep","WCS","ChaCha","Samba","Rumba","PasoDoble","Jive"]
+    #dances = ["QuickStep","PasoDoble","Jive"]
+    dances = ["Waltz","Tango","VienneseWaltz","QuickStep","WCS","ChaCha","Samba","Rumba","PasoDoble","Jive"]
 
-fig = Figlet(font='standard')
-for idx in range(len(dances)):
-  dance=dances[idx]
-  #print "\t\t*** "+dance.upper()+" ***"
-  print fig.renderText(dance)
-  musicPath=musicDir+dance
-  playlist=[]
-  for (dirpath, dirnames, filenames) in walk(musicPath):
-    playlist.extend(filenames)
-    break
-  if dance=="Waltz":
-      print(" [Waltz has an extra selection for volume adjustment]")
-      cnt = -1
-  elif dance=="PasoDoble":
-      song="Get Ready for Paso.mp3"
-      nextsong=dirpath+"/"+song
-      player=vlc.MediaPlayer(nextsong)
-      player.audio_set_volume(100)
-      infoStr = displayInfo(player)
-      if "Unknown" not in infoStr:
-        print(infoStr)
-      else:
-        print(infoStr)
-        #print("Filename: "+song)
-      player.play()
-      time.sleep(17)
-      for level in range(100,10,-10):
-          player.audio_set_volume(level)
-          time.sleep(1)
-      player.stop()
-      time.sleep(5)
-      cnt = numSel-1 # play only one Paso Doble
-  else:
-      cnt = 0
-  rplaylist=randomList(playlist)
-  for song in rplaylist:
-    cnt = cnt + 1
-    if cnt > numSel:
-        break
-    nextsong=dirpath+"/"+song
-    player=vlc.MediaPlayer(nextsong)
-    player.audio_set_volume(100)
-    infoStr = displayInfo(player)
-    if "Unknown" not in infoStr:
-      print(infoStr)
+    fig = Figlet(font='standard')
+    for idx in range(len(dances)):
+        dance=dances[idx]
+        #print "\t\t*** "+dance.upper()+" ***"
+        print fig.renderText(dance)
+        musicPath=musicDir+dance
+        playlist=[]
+        for (dirpath, dirnames, filenames) in walk(musicPath):
+            playlist.extend(filenames)
+            break
+        if dance=="Waltz":
+            print(" [Waltz has an extra selection for volume adjustment]")
+            cnt = -1
+        elif dance=="PasoDoble":
+            song="Get Ready for Paso.mp3"
+            nextsong=dirpath+"/"+song
+            player=vlc.MediaPlayer(nextsong)
+            player.audio_set_volume(100)
+            infoStr = displayInfo(player)
+            if "Unknown" not in infoStr:
+                print(infoStr)
+            else:
+                print(infoStr)
+                #print("Filename: "+song)
+            player.play()
+            time.sleep(17)
+            for level in range(100,10,-10):
+                player.audio_set_volume(level)
+                time.sleep(1)
+            player.stop()
+            time.sleep(5)
+            cnt = numSel-1 # play only one Paso Doble
+        else:
+            cnt = 0
+        rplaylist=randomList(playlist)
+        for song in rplaylist:
+            cnt = cnt + 1
+            if cnt > numSel:
+                break
+            nextsong=dirpath+"/"+song
+            player=vlc.MediaPlayer(nextsong)
+            player.audio_set_volume(100)
+            infoStr = displayInfo(player)
+            if "Unknown" not in infoStr:
+                print(infoStr)
+            else:
+                print(infoStr)
+                #print("Filename: "+song)
+            player.play()
+            time.sleep(1)
+            while True:
+                if player.is_playing():
+                    continue
+                else:
+                    break
+
+if __name__=='__main__':
+    if len(sys.argv) > 2:
+        print "Script to play dance music for practice"
+        print "Usage: "+sys.argv[0]+" [number]"
+        print "  [number] is musical selections per dance."
+        print "  Default is 2 if no argument provided."
+        exit(1)
+    elif len(sys.argv) == 2:
+        if not is_intString(sys.argv[1]):
+            print "The number of musical selection per dance must be an integer."
+            exit(1)
+        numSel = int(sys.argv[1])
     else:
-      print(infoStr)
-      #print("Filename: "+song)
-    player.play()
-    time.sleep(1)
-    while True:
-      if player.is_playing():
-        continue
-      else:
-        break
+        numSel = 2
+
+    play_music(numSel)
